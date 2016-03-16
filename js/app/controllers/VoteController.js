0aef3e5ac1439771e81f71823e656f7068088513
jakebrady5 <jakebrady5@gmail.com>
2016-03-15 20:52:32 -0400

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,20 @@
 function VoteController() {
     this.votes = 0;
+    this.add = 0;
+    this.remove = 1;
 
     this.incrementVotes = function () {
-
+      if (this.add%2 ===0){
+        this.votes++;
+        this.add++;
+      }
     };
 
     this.decrementVotes = function () {
-
+      if (this.remove === 1){
+        this.votes--;
+        this.remove--;
+      }
     };
 }
 
