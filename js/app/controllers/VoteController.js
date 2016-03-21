2e668ebf9dacab9e07bac0caccb2986ebe69a3cc
github username <mendelblesofsky@gmail.com>
2016-03-20 21:40:29 -0400

    Done

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -2,11 +2,13 @@ function VoteController() {
     this.votes = 0;
 
     this.incrementVotes = function () {
-
+    	if (this.votes <= 0)
+    		this.votes++;
     };
 
     this.decrementVotes = function () {
-
+    	if (this.votes >= 0)
+    		this.votes--;
     };
 }
 
