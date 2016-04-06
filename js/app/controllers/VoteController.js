cee5c97aff68766c86d6a9927137e3c5f9e458ea
alexbarron <alexbarron@gmail.com>
2016-04-06 11:35:02 -0700

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,26 @@
 function VoteController() {
     this.votes = 0;
+    this.voted = false;
+    this.decremented = false;
 
     this.incrementVotes = function () {
-
+      if (this.voted){
+        this.voted = false;
+        this.votes--;
+      } else {
+        this.voted = true;
+        this.votes++;
+      }
     };
 
     this.decrementVotes = function () {
-
+      if (this.decremented){
+        this.decremented = false;
+        this.votes++;
+      } else {
+        this.decremented = true;
+        this.votes--;
+      }
     };
 }
 
