84ea285645c176bb6139971f1c9c2e36c83c5052
Rawradin <edgar.rawr@gmail.com>
2016-04-11 17:02:56 -0700

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,19 @@
 function VoteController() {
     this.votes = 0;
+    this.direction = '';
 
     this.incrementVotes = function () {
-
+    	if (this.direction != 'up'){
+	    	this.votes++;
+	    	this.direction = 'up';
+	    }
     };
 
     this.decrementVotes = function () {
-
+    	if (this.direction != 'down'){
+	    	this.votes--;
+	    	this.direction = 'down';
+	    }
     };
 }
 
