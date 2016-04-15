704f2633434cae1101237dabf3072632358b6730
zachnewburgh <zach.newburgh@gmail.com>
2016-04-15 13:16:10 -0400

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,24 @@
 function VoteController() {
     this.votes = 0;
+    this.increased = false;
+    this.decreased = false;
 
     this.incrementVotes = function () {
-
+      if (this.increased === false || this.votes === 0) {
+        this.votes++;
+        this.increased = true;
+      } else {
+        alert("You've already added a vote!");
+      }
     };
 
     this.decrementVotes = function () {
-
+      if (this.decreased === false || this.votes === 0) {
+        this.votes--;
+        this.decreased = true;
+      } else {
+        alert("You've already removed a vote!");
+      }
     };
 }
 
