f3bc76f5adf897f035377dbd999f209b50fcd7b2
Brett <brettrheenan@gmail.com>
2016-04-27 16:54:46 +0200

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,13 +1,32 @@
 function VoteController() {
-    this.votes = 0;
+	var that = this;
+  that.votes = 0;
+  that.status = true;
+  that.clicked = false;
 
-    this.incrementVotes = function () {
+  this.incrementVotes = function () {
+  	that.votes++;
+  };
 
-    };
+  this.decrementVotes = function () {
+  	that.votes--;
+  };
 
-    this.decrementVotes = function () {
+  this.toggleIncrement = function (status) {
+  	if (status === true) {
+  		that.status = false;
+  	} else {
+  		that.status = true;
+  	};
+  };
 
-    };
+  this.toggleDecrement = function (clicked) {
+  	if (clicked === false) {
+  		that.clicked = true;
+  	} else {
+  		that.clicked = false;
+  	};
+  };
 }
 
 angular
