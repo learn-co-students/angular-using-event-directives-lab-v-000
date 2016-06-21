2d49d8514a9f281995305bc0e11a4c986d6bbef0
Kenneth Amper <kenn.amp@gmail.com>
2016-06-21 12:50:44 -0700

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,22 @@
 function VoteController() {
     this.votes = 0;
+		var upVoteFlag = false;
+		var downVoteFlag = false;
 
     this.incrementVotes = function () {
-
+			if ( upVoteFlag == false ) {
+				this.votes = this.votes + 1;
+				upVoteFlag = true;
+				downVoteFlag = false;
+			}
     };
 
     this.decrementVotes = function () {
-
+			if ( downVoteFlag == false ) {
+				this.votes = this.votes - 1;
+				upVoteFlag = false;
+				downVoteFlag = true;
+			}
     };
 }
 
