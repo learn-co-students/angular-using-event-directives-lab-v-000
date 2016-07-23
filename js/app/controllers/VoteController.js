c314c74a0a08e3631eeb955ffe63fea3c609ed08
thebrianemory <the@brianemory.com>
2016-07-22 18:25:27 -0700

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -2,11 +2,23 @@ function VoteController() {
     this.votes = 0;
 
     this.incrementVotes = function () {
-
+      if (this.upvoted === 'true') {
+        this.votes--;
+        this.upvoted = 'false';
+      } else {
+        this.votes++;
+        this.upvoted = 'true';
+      }
     };
 
     this.decrementVotes = function () {
-
+      if (this.downvoted === 'true') {
+        this.votes++;
+        this.downvoted = 'false';
+      } else {
+        this.votes--;
+        this.downvoted = 'true';
+      }
     };
 }
 
