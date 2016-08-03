9a0ee711dd0e9bd8f33bf52daf6b5549e1be9d87
Peter Jacobson <github email address>
2016-08-03 15:15:01 -0400

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -2,11 +2,17 @@ function VoteController() {
     this.votes = 0;
 
     this.incrementVotes = function () {
-
+      this.votes++;
+       if (this.votes >= this.votes +1) {
+        this.votes = 0;
+      }
     };
 
     this.decrementVotes = function () {
-
+      this.votes--;
+      if (this.votes <= this.votes -1) {
+        this.votes = 0;
+      }
     };
 }
 
