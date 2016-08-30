655aba29bebb3d69c8f1c9e336f5d51f6ab2dab4
agdavid <antonio.g.david@gmail.com>
2016-08-30 08:53:42 -0400

    code bonus toggle for limited up and down vote

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,13 +1,34 @@
 function VoteController() {
     this.votes = 0;
+    this.voteAdded = 0;
+    this.voteRemoved = 0;
 
     this.incrementVotes = function () {
-      this.votes++; 
+      if (this.voteAdded === 0) {
+        // user has not incremented vote
+        this.votes++;
+        this.voteAdded++;  
+      } else {
+        // user has previously incremented vote
+        this.votes--;
+        this.voteAdded--; 
+        alert("You can only add one vote. We will remove your previously added vote.");
+      }
     };
 
     this.decrementVotes = function () {
-      this.votes--;
+      if (this.voteRemoved === 0) {
+        // user has not decremented vote
+        this.votes--;
+        this.voteRemoved++;  
+      } else {
+        // user has previously decremented vote
+        this.votes++;
+        this.voteRemoved--;
+        alert("You can only remove one vote. We will add back your previously removed vote.");
+      }
     };
+      
 }
 
 angular
