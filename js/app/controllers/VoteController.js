8cfdb2703d527421dea5f5a2b747253a41e38200
aprietof <adrianprieto00@gmail.com>
2016-09-23 12:44:38 -0400

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,13 +1,32 @@
 function VoteController() {
     this.votes = 0;
+    var votesInit = this.votes;
+    var voteUp = false;
+    var voteDown = false;
 
     this.incrementVotes = function () {
-
+      if (voteUp == false && voteDown == false) {
+        this.votes++;
+        voteUp = true;
+        voteDown = false;
+      } else if (voteUp == false && voteDown == true) {
+        this.votes++;
+        voteUp = true;
+        voteDown = false;
+      };
     };
 
     this.decrementVotes = function () {
-
-    };
+      if (voteDown == false && voteUp == false) {
+        this.votes--;
+        voteDown = true;
+        voteUp = false;
+      } else if (voteDown == false && voteUp == true) {
+        this.votes--;
+        voteDown = true;
+        voteUp = false;
+      }
+    }
 }
 
 angular
