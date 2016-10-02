bee05aecc0716ee833d31b979ff3dcdbf33aed9e
Saturn226 <serenehopes@gmail.com>
2016-10-02 01:58:57 -0400

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,26 @@
 function VoteController() {
     this.votes = 0;
+    this.voted = true;
 
     this.incrementVotes = function () {
-
+      // if (this.voted === false){
+      //   this.votes++;
+      //   this.voted = true;
+      // }
+      // else{
+      //   this.votes--;
+      //   this.voted = false;
+      // }
+      this.votes++;
     };
 
     this.decrementVotes = function () {
-
+      // if(this.voted === true)
+      // {
+      //   this.votes--
+      //   this.voted = false;
+      // }
+      this.votes--;
     };
 }
 
