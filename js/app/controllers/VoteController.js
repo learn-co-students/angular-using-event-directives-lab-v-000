8ef8482d9675ca54cb1850d8ba749b7407ce5c48
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-04 22:28:18 -0700

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,11 +1,18 @@
 function VoteController() {
+
     this.votes = 0;
+    var startingVote = this.votes;
 
     this.incrementVotes = function () {
-
+      if(this.votes <= startingVote){
+        this.votes+=1;
+      }
     };
 
     this.decrementVotes = function () {
+      if (this.votes >= startingVote){
+        this.votes-=1;
+      }
 
     };
 }
