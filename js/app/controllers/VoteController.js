caace35a534a9e940916488fb55b44791abac778
Chris-Sexton <champlain2013@gmail.com>
2016-10-06 16:40:46 -0400

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -2,11 +2,15 @@ function VoteController() {
     this.votes = 0;
 
     this.incrementVotes = function () {
-
+      if (this.votes === 0 ) {
+        this.votes++
+      } else {
+        this.votes--
+      }
     };
 
     this.decrementVotes = function () {
-
+      this.votes--
     };
 }
 
