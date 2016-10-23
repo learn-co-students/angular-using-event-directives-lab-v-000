bef05747dbf0e59ab81af45c12f837ff92f69719
peterberwind <peterberwind@gmail.com>
2016-10-23 17:22:36 -0400

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,23 @@
 function VoteController() {
     this.votes = 0;
+    var hasVoted = false;
+    var hasRemovedVote = false;
 
     this.incrementVotes = function () {
-
+      if(hasVoted == false) {
+        this.votes++
+        hasVoted = true;
+      } else {
+        this.votes--
+        hasVoted = false;
+      }
     };
 
     this.decrementVotes = function () {
-
+      if(hasRemovedVote == false) {
+        this.votes--
+        hasRemovedVote = true;
+      }
     };
 }
 
