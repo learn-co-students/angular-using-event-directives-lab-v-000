7726b7e260242fd6b85295912d7061d358eb4672
tttra <tothutra@gmail.com>
2016-10-23 21:11:42 -0400

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,11 +1,19 @@
 function VoteController() {
     this.votes = 0;
+    this.turn = "on"
 
     this.incrementVotes = function () {
-
+      if (this.turn == "on") {
+        this.votes += 1
+        this.turn = "off"
+      }
     };
 
     this.decrementVotes = function () {
+      if (this.turn == "off") {
+        this.votes -= 1
+        this.turn = "on"        
+      }
 
     };
 }
