f516d6e7b231034ddb3c89c7ffe21a44267b6529
arturusfury <bri.reynolds@gmail.com>
2016-10-31 17:48:35 -0500

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,26 @@
 function VoteController() {
     this.votes = 0;
+    this.votedUp = false;
+    this.votedDown = false;
 
     this.incrementVotes = function () {
-
+      if (this.votedUp == true) {
+        alert('You have already voted');
+      }
+      else {
+        this.votes++;
+        this.votedUp = true;
+      }
     };
 
     this.decrementVotes = function () {
-
+      if (this.votedDown == true) {
+        alert('You have already voted');
+      }
+      else {
+        this.votes--;
+        this.votedDown = true;
+      }
     };
 }
 
