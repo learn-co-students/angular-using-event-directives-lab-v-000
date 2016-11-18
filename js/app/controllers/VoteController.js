7d87a52168efb81204a9d9bf1b9656954db63a10
github username <github email address>
2016-11-18 10:30:56 -0500

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,14 +1,26 @@
 function VoteController() {
     this.votes = 0;
+    var incrementClicked = false;
+    var decrementClicked = false;
 
-    this.incrementVotes = function () {
-
+  this.incrementVotes = function () {
+      if (incrementClicked == false) {
+        this.votes++;
+        incrementClicked = true;
+      } else if (incrementClicked == true) {
+        this.votes--;
+        incrementClicked = false;
+      }
     };
 
     this.decrementVotes = function () {
-
+      if (decrementClicked == false) {
+        this.votes--;
+        decrementClicked = true;
+      }
     };
-}
+  };
+
 
 angular
     .module('app')
