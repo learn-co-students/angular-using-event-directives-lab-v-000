34d18f7bc3a158b010cb07436e4a0667716b2dab
kade042 <dereje88@gmail.com>
2016-11-19 12:39:54 -0700

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,13 @@
 function VoteController() {
     this.votes = 0;
 
-    this.incrementVotes = function () {
+    this.incrementVotes = function (e) {
 
+      this.votes += 1;
     };
 
     this.decrementVotes = function () {
-
+      this.votes -= 1;
     };
 }
 
