b36c95fcdbd2f269dcdca38f0adf62cd514427f5
DownsideupmediaUSA <brandondecarlo@live.com>
2016-12-31 12:58:49 -0500

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,14 @@
 function VoteController() {
+  var vm = this;
+  
     this.votes = 0;
 
     this.incrementVotes = function () {
-
+        vm.votes++;
     };
 
     this.decrementVotes = function () {
-
+        vm.votes--;
     };
 }
 
