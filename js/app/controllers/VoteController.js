45ae30f15a3164fec8a3a8630be7b4cc57fa4fdd
hilaryml <hilyttle@gmail.com>
2017-02-08 15:23:50 -0500

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,25 @@
 function VoteController() {
     this.votes = 0;
+    this.toggle = false;
 
     this.incrementVotes = function () {
-
+      if (this.toggle === false) {
+        this.votes ++
+        this.toggle = true
+      } else {
+        this.votes --
+        this.toggle = false
+      }
     };
 
     this.decrementVotes = function () {
-
+      if (this.toggle === false) {
+        this.votes --
+        this.toggle = true
+      } else {
+        this.votes ++
+        this.toggle = false
+      }
     };
 }
 
