2b87a042d0900af293056471a91a1c88abf8ef9f
Clare <clare.e.horton@gmail.com>
2017-02-10 01:35:04 -0600

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,13 +1,28 @@
-function VoteController() {
+function VoteController($scope) {
     this.votes = 0;
 
-    this.incrementVotes = function () {
+    $scope.addDisabled = false;
+    $scope.removeDisabled = false;
 
+    this.incrementVotes = function () {
+      this.votes += 1;
+      $scope.disableButton('add');
     };
 
     this.decrementVotes = function () {
-
+      this.votes -= 1;
+      $scope.disableButton('remove');
     };
+
+    $scope.disableButton = function (param) {
+      if (param === 'add') {
+        $scope.addDisabled = true;
+        $scope.removeDisabled = false;
+      } else {
+        $scope.removeDisabled = true;
+        $scope.addDisabled = false;
+      }
+    }
 }
 
 angular
